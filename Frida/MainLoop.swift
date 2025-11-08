8be00cb6de27b2c1500d968b0b72c9a3909f1c1c
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-11-08 23:03:08 +0100

    Add minimal GLib.MainLoop bindings

---
+++ Frida/MainLoop.swift
@@ -0,0 +1,21 @@
+import Frida_Private
+
+public final class MainLoop: CustomStringConvertible {
+    internal let handle: OpaquePointer
+
+    public init() {
+        self.handle = g_main_loop_new(nil, 0)
+    }
+
+    deinit {
+        g_main_loop_unref(handle)
+    }
+
+    public var description: String {
+        "Frida.MainLoop()"
+    }
+
+    public func run() {
+        g_main_loop_run(handle)
+    }
+}
