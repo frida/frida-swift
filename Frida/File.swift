01a9dc41f3e81f35704fb5e972a3468ea9ad31d2
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-11-08 23:02:12 +0100

    Add minimal GLib.File bindings

---
+++ Frida/File.swift
@@ -0,0 +1,36 @@
+import Frida_Private
+
+public final class File: @unchecked Sendable, CustomStringConvertible, Equatable, Hashable {
+    internal let handle: OpaquePointer
+
+    public convenience init(file: String) throws {
+        Runtime.ensureInitialized()
+
+        self.init(handle: g_file_new_for_path(file)!)
+    }
+
+    public init(handle: OpaquePointer) {
+        self.handle = handle
+    }
+
+    deinit {
+        g_object_unref(gpointer(handle))
+    }
+
+    public var description: String {
+        "Frida.File()"
+    }
+
+    public static func == (lhs: File, rhs: File) -> Bool {
+        lhs.handle == rhs.handle
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(UInt(bitPattern: handle))
+    }
+
+    public var path: String? {
+        guard let raw = g_file_get_path(handle) else { return nil }
+        return Marshal.stringFromCString(raw)
+    }
+}
