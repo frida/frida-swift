347c20eafadb6eb8df3ec098da098630e0ef4d82
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-12-01 22:46:34 +0100

    error: Add `description` property

--- Frida/Error.swift
+++ Frida/Error.swift
@@ -14,4 +14,29 @@ public enum Error: Swift.Error, Codable {
     case protocolViolation(String)
     case transport(String)
     case rpcError(message: String, stackTrace: String?)
+
+    public var description: String {
+        switch self {
+        case let .serverNotRunning(message),
+             let .executableNotFound(message),
+             let .executableNotSupported(message),
+             let .processNotFound(message),
+             let .processNotResponding(message),
+             let .invalidArgument(message),
+             let .invalidOperation(message),
+             let .permissionDenied(message),
+             let .addressInUse(message),
+             let .timedOut(message),
+             let .notSupported(message),
+             let .protocolViolation(message),
+             let .transport(message):
+            return message
+
+        case let .rpcError(message: message, stackTrace: stackTrace):
+            if let stackTrace, !stackTrace.isEmpty {
+                return "\(message)\n\n\(stackTrace)"
+            }
+            return message
+        }
+    }
 }
